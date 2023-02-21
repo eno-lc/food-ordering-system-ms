f7965ecff585a74615d5682f0e531e91690094e3
eno-lc <eh52370@ubt-uni.net>
2023-02-21 14:45:09 +0100

    added entities for customer and restaurant and its repository and mappers

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Customer.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Customer.java
@@ -3,5 +3,13 @@ package com.food.ordering.system.order.service.domain.entity;
 import com.food.ordering.system.domain.entity.AggregateRoot;
 import com.food.ordering.system.domain.valueobject.CustomerId;
 
+import java.util.UUID;
+
 public class Customer extends AggregateRoot<CustomerId> {
+
+    public Customer() {
+    }
+    public Customer(CustomerId id) {
+        super.setId(id);
+    }
 }
