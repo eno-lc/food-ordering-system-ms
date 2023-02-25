2b768f51ac28326aef5ec0a07a67b9242127d14c
Enis Halilaj <eh52370@ubt-uni.net>
2023-02-25 13:30:42 +0100

    added the bean configuration and resources for the paymentServiceApplication

--- payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/PaymentServiceApplication.java
+++ payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/PaymentServiceApplication.java
@@ -5,15 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-/**
- * @author Enis Halilaj
- * @since 1.0.0
- * @EnableJpaRepositories is used to enable JPA repositories in the application. In this case, it is used to enable JPA repositories in the data access layer.
- * @EntityScan is used to scan JPA entities. In this case, it is used to scan JPA entities in the data access layer.
- * @SpringBootApplication is used to enable auto-configuration of the Spring Application Context.
- * @SpringBootApplication is equivalent to using @Configuration, @EnableAutoConfiguration and @ComponentScan with their default attributes.
- */
-
 @EnableJpaRepositories(basePackages = "com.food.ordering.system.payment.service.dataaccess")
 @EntityScan(basePackages = "com.food.ordering.system.payment.service.dataaccess")
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
